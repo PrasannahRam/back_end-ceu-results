3175cd9cbb424523a14d70b377f1bdd3aeb7733c
PrasannahRam <rprasu2005@gmail.com>
2025-08-05 11:15:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');       // import express
+const app = express();                    // create an Express app
+const mysql = require("mysql2");
+const PORT = 3000;                        // define a port
+const cors = require('cors'); // add this
+require("dotenv").config();
+
+app.use(cors());    
+app.use(express.json());                  // enable parsing JSON body in requests
+console.log(process.env.MYSQL_URL);
+
+const db = mysql.createConnection(process.env.MYSQL_URL);
+
+
+// Define a basic route
+app.get('/', (req, res) => {
+  res.send(`Hello from Node server!`);
+});
+
+app.post('/api/result',(req,res)=>{
+  const idtype = req.body.idtype
+  const exam = req.body.exam
+  const id = req.body.id
+  const year = req.body.year
+    console.log(req.body);
+    
+    db.query(
+    `SELECT * FROM ${exam}${year} WHERE ${idtype} = ?`,
+    [id],
+    (err, result) => {
+      if (err) return res.status(500).json({ error: err.message });
+      if (result.length === 0) return res.status(404).json({ message: "No result found" });
+      res.json(result[0]);
+      console.log(res.json());
+      
+    }
+  )
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
